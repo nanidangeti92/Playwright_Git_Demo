3d37807d16980c9af49b4e96ff5dbd1a1d82c9c5
nanidangeti <nanid9611@gmail.com>
2026-09-10 17:00:21 +0530

    Login Test Added

---
+++ tests/login.spec.js
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('Login Test', async ({ page }) => {
+    await page.goto("http://orangehrm.qedgetech.com")
+    await page.locator("#txtUsername").fill("Admin")
+    await page.locator("#txtPassword").fill("Qedge123!@#")
+    await page.getByRole("button",{name:"LOGIN"}).click()
+  
+    
+})
