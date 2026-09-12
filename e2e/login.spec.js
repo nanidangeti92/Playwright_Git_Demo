245a8bdce1fb3a8a852bac50f124d1e993cf8d20
nanidangeti <nanid9611@gmail.com>
2026-09-12 12:37:19 +0530

    Login Test updated

---
+++ e2e/login.spec.js
@@ -0,0 +1,30 @@
+import {test,expect} from "@playwright/test"
+
+test ('Login Test',async({page})=>{
+ await page.goto("http://orangehrm.qedgetech.com")
+ await page.locator("#txtUsername").fill("Admin")
+ await page.locator("#txtPassword").fill("Qedge123!@#")
+ await page.locator("#btnLogin").click()
+ await expect (page.getByRole('link',{name:'Admin'})).toBeVisible()
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
